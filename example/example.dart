8d3ec3fcba1c3bc71730a3c64d61b98c08826dca
h102136 <h102136@gmail.com>
2024-09-01 21:26:19 +0930

    api and test case update

--- example/example.dart
+++ example/example.dart
@@ -1,28 +1,28 @@
-import 'package:s2geometry_dart/src/s2cell.dart';
-import 'package:s2geometry_dart/src/s2cell_utils.dart';
+import 'package:s2geometry_dart/s2geometry.dart';
 void main(){
   var lat = 40.2574448;
   var lng = -111.7089464;
   var level = 15;
   
-  var key = S2CellUtils.latLngToKey(lat, lng, level);
+  var key = S2.latLngToKey(lat, lng, level);
   print(key);
 
-  var id = S2CellUtils.keyToId(key);
+  var id = S2.keyToId(key);
   print(id);
 
-  var key2 = S2CellUtils.idToKey(id);
+  var key2 = S2.idToKey(id);
   print(key2);
 
-  var neighbors = S2Cell.latLngToNeighborKeys(lat, lng, level);
+  var neighbors = S2.latLngToNeighborKeys(lat, lng, level);
   print(neighbors);
 
-  var nextKey = S2CellUtils.nextKey(key);
+  var nextKey = S2.nextKey(key);
   print(nextKey);
-  var prevKey = S2CellUtils.prevKey(key);
+  var prevKey = S2.prevKey(key);
   print(prevKey); 
 
-  var backTenKeys = S2CellUtils.stepKey(key, -10);
+  var backTenKeys = S2.stepKey(key, -10);
   print(backTenKeys); 
 
 }
+
