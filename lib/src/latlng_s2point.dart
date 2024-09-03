36a700ef0f8a6db43704df5adf4715917c506183
h102136 <h102136@gmail.com>
2024-09-03 14:31:09 +0930

    code review and update

--- lib/src/latlng_s2point.dart
+++ lib/src/latlng_s2point.dart
@@ -1,12 +1,39 @@
 import 'dart:math' as math;
 
+/* source code from js
+
+(function (exports) {
+'use strict';
+var S2 = exports.S2 = { L: {} };
+
+S2.L.LatLng = function (/*Number*/ rawLat, /*Number*/ rawLng, /*Boolean*/ noWrap) {
+  var lat = parseFloat(rawLat, 10);
+  var lng = parseFloat(rawLng, 10);
+
+  if (isNaN(lat) || isNaN(lng)) {
+    throw new Error('Invalid LatLng object: (' + rawLat + ', ' + rawLng + ')');
+  }
+
+  if (noWrap !== true) {
+    lat = Math.max(Math.min(lat, 90), -90);                 // clamp latitude into -90..90
+    lng = (lng + 180) % 360 + ((lng < -180 || lng === 180) ? 180 : -180);   // wrap longtitude into -180..180
+  }
+
+  return { lat: lat, lng: lng };
+};
+
+S2.L.LatLng.DEG_TO_RAD = Math.PI / 180;
+S2.L.LatLng.RAD_TO_DEG = 180 / Math.PI;
+*/
+
+// LatLng class ensures that lat and lng values are valid
 class LatLng {
-  double lat; // Latitude
-  double lng; // Longitude
+  double lat; 
+  double lng; 
 
   // Conversion constants between degrees and radians
-  static const double degToRad = math.pi / 180; // Degrees to radians
-  static const double radToDeg = 180 / math.pi; // Radians to degrees
+  static const double degToRad = math.pi / 180; 
+  static const double radToDeg = 180 / math.pi; 
 
   // Constructor
   LatLng(dynamic rawLat, dynamic rawLng, {bool noWrap = false}) :
@@ -14,41 +41,63 @@ class LatLng {
     lat = double.tryParse(rawLat.toString()) ?? double.nan, // Convert to double, set to NaN if invalid
     lng = double.tryParse(rawLng.toString()) ?? double.nan {
     
-    // Check if lat and lng are valid
-    if (lat.isNaN || lng.isNaN) {
-      throw ArgumentError('Invalid LatLng object: ($rawLat, $rawLng)'); // Throw error if invalid
+      // Check if lat and lng are valid
+      if (lat.isNaN || lng.isNaN) {
+        throw ArgumentError('Invalid LatLng object: ($rawLat, $rawLng)'); // Throw error if invalid
+        }
+
+      if (!noWrap) {
+        lat = lat.clamp(-90, 90);
+        lng = (lng + 180) % 360 - 180; // Wrap lng to -180..180 if noWrap is false
+        }
     }
 
-    // Wrap lng to -180..180 if noWrap is false
-    if (!noWrap) {
-      lat = lat.clamp(-90, 90);
-      lng = (lng + 180) % 360 - 180;
+    // Override toString method to display lat and lng values
+    @override
+    String toString() {
+      return 'LatLng(lat: $lat, lng: $lng)';
     }
-  }
-
-  // Override toString method to display lat and lng values
-  @override
-  String toString() {
-    return 'LatLng(lat: $lat, lng: $lng)';
-  }
 }
+/* source code from js
+S2.LatLngToXYZ = function(latLng) {
+  var d2r = S2.L.LatLng.DEG_TO_RAD;
 
+  var phi = latLng.lat*d2r;
+  var theta = latLng.lng*d2r;
+
+  var cosphi = Math.cos(phi);
+
+  return [Math.cos(theta)*cosphi, Math.sin(theta)*cosphi, Math.sin(phi)];
+};
+
+S2.XYZToLatLng = function(xyz) {
+  var r2d = S2.L.LatLng.RAD_TO_DEG;
+
+  var lat = Math.atan2(xyz[2], Math.sqrt(xyz[0]*xyz[0]+xyz[1]*xyz[1]));
+  var lng = Math.atan2(xyz[1], xyz[0]);
+
+  return S2.L.LatLng(lat*r2d, lng*r2d);
+};
+*/
+
+// S2Point class contains static methods to convert between LatLng and 3D coordinates(xyz)
 class S2Point{
+  
   // Convert lat,lng to 3D point (x, y, z)
   static List<double> latLngToXYZ(LatLng latLng) {
     double d2r = LatLng.degToRad;
 
-    double phi = latLng.lat * d2r; // Latitude in radians
-    double theta = latLng.lng * d2r; // Longitude in radians
+    double phi = latLng.lat * d2r; 
+    double theta = latLng.lng * d2r; 
 
     double cosphi = math.cos(phi);
 
     return [
-      math.cos(theta) * cosphi, // x
-      math.sin(theta) * cosphi, // y
-      math.sin(phi) // z
-    ];
-  }
+      math.cos(theta) * cosphi, //x
+      math.sin(theta) * cosphi, //y
+      math.sin(phi) //z
+      ];
+    }
 
   // Convert 3D point (x, y, z) to lat,lng
   static LatLng xyzToLatLng(List<double> xyz) {
@@ -57,6 +106,6 @@ class S2Point{
     double lat = math.atan2(xyz[2], math.sqrt(xyz[0] * xyz[0] + xyz[1] * xyz[1]));
     double lng = math.atan2(xyz[1], xyz[0]);
 
-    return LatLng(lat * r2d, lng * r2d);
+    return LatLng(lat * r2d, lng * r2d); 
   }
 }
--- test/latlng_s2point_test.dart
+++ test/latlng_s2point_test.dart
@@ -2,43 +2,51 @@ import 'package:test/test.dart';
 import 'package:s2geometry_dart/src/latlng_s2point.dart';
 
 void main() {
+  //test cases for LatLng class
   group('LatLng', () {
-    test('should clamp latitude to -90..90', () {
+
+    test('should create a LatLng object with lat,lng', () {
+      final latLng = LatLng(45.0, 90.0);
+      expect(latLng.lat, 45.0);  
+      expect(latLng.lng, 90.0);  
+    }); 
+
+    test('should throw an error for invalid lat,lng values', () {
+      expect(() => LatLng('invalid', 90.0), throwsArgumentError);
+      expect(() => LatLng(45.0, 'invalid'), throwsArgumentError);
+    });
+
+    test('should clamp lat to -90..90', () {
       final latLng = LatLng(100.0, 90.0);
       expect(latLng.lat, 90.0);  
       expect(latLng.lng, 90.0);  
     });
 
-    test('should wrap longitude to -180..180', () {
+    test('should wrap lng to -180..180', () {
       final latLng = LatLng(45.0, 200.0);
       expect(latLng.lat, 45.0);  
       expect(latLng.lng, -160.0); 
     });
 
-    test('should wrap longitude to -180..180 and latitude to -90..90 test2(-100,-200 =>-90,160)', () {
+    test('should wrap lng to -180..180 and lat to -90..90 (-100,-200 =>-90,160)', () {
       final latLng = LatLng(-100.0, -200.0);
       expect(latLng.lat, -90.0);  
       expect(latLng.lng, 160.0); 
     });
 
-    test('should wrap longitude to -180..180 and latitude to -90..90 test3(11111,11111 =>90,-49)', () {
+    test('should wrap longitude to -180..180 and latitude to -90..90 (11111,11111 =>90,-49)', () {
       final latLng = LatLng(11111.0, 11111.0);
       expect(latLng.lat, 90.0);  
       expect(latLng.lng, -49.0); 
     });
 
-    test('should throw an error for invalid lat/lng values', () {
-      expect(() => LatLng('invalid', 90.0), throwsArgumentError);
-      expect(() => LatLng(45.0, 'invalid'), throwsArgumentError);
-    });
-
-    test('should not wrap latitude and longitude if noWrap is true', () {
+    test('should not wrap lat and lng if noWrap is true', () {
       final latLng = LatLng(-100.0, -200.0, noWrap: true);
       expect(latLng.lat, -100.0);  
       expect(latLng.lng, -200.0); 
     });
-
-    test('should convert lat/lng (45.0, 90.0) to XYZ correctly', () {
+    // test cases for S2Point class
+    test('should convert lat,lng (45.0, 90.0) to xyz correctly', () {
       final latLng = LatLng(45.0, 90.0);
       final result = S2Point.latLngToXYZ(latLng);
 
@@ -47,7 +55,7 @@ void main() {
       expect(result[2], closeTo(0.70710678118, 1e-10));  // z
     });
 
-    test('should convert lat/lng (-45.0, -90.0)to XYZ correctly', () {
+    test('should convert lat,lng (-45.0, -90.0)to xyz correctly', () {
       final latLng = LatLng(-45.0, -90.0);
       final result = S2Point.latLngToXYZ(latLng);
 
@@ -56,7 +64,7 @@ void main() {
       expect(result[2], closeTo(-0.70710678118, 1e-10));  
     });
 
-    test('should convert XYZ to lat/lng (45.0, 90.0)correctly', () {
+    test('should convert xyz to lat,lng (45.0, 90.0)correctly', () {
       final xyz = [0.0, 0.70710678118, 0.70710678118];
       final result = S2Point.xyzToLatLng(xyz);
 
@@ -64,7 +72,7 @@ void main() {
       expect(result.lng, closeTo(90.0, 1e-10)); 
     });
 
-    test('should convert XYZ to lat/lng (-45.0, -90.0)correctly', () {
+    test('should convert xyz to lat,lng (-45.0, -90.0)correctly', () {
       final xyz = [0.0, -0.70710678118, -0.70710678118];
       final result = S2Point.xyzToLatLng(xyz);
 
