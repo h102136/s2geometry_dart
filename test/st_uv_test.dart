5528b21ba7beb28af86700f40f620189dacb564b
Henry Lin <129135909+h102136@users.noreply.github.com>
2024-09-05 20:27:19 +0930

    Henry (#3)
    * latlng test update
    
    * face_uv update
    
    * st_uv update
    
    * update

--- test/st_uv_test.dart
+++ test/st_uv_test.dart
@@ -2,8 +2,8 @@ import 'package:test/test.dart';
 import 'package:s2geometry_dart/src/st_uv.dart'; 
 
 void main() {
-  group('STUVConversion tests', () {
-    test('singleSTtoUV converts st to uv correctly', () {
+  group('ST,UV conversion', () {
+    test('singleSTtoUV converts single st to single uv correctly', () {
       expect(singleSTtoUV(0), closeTo(-1, 1e-9));
       expect(singleSTtoUV(0.25), closeTo(-0.4166666666666666, 1e-9));
       expect(singleSTtoUV(0.5), closeTo(0, 1e-9));
@@ -11,14 +11,14 @@ void main() {
       expect(singleSTtoUV(1), closeTo(1, 1e-9));
     });
 
-    test('STToUV converts st array to uv array correctly', () {
+    test('stToUV converts st array to uv array correctly', () {
       expect(stToUV([0.5, 0.5]), [closeTo(0, 1e-9), closeTo(0, 1e-9)]);
       expect(stToUV([1, 1]), [closeTo(1, 1e-9), closeTo(1, 1e-9)]);
       expect(stToUV([0, 0]), [closeTo(-1, 1e-9), closeTo(-1, 1e-9)]);
       expect(stToUV([0.75, 0.25]), [closeTo(0.4166666666666666, 1e-9), closeTo(-0.4166666666666666, 1e-9)]);
     });
 
-    test('singleUVtoST converts uv to st correctly', () {
+    test('singleUVtoST converts single uv to single st correctly', () {
       expect(singleUVtoST(0), closeTo(0.5, 1e-9));
       expect(singleUVtoST(0.333), closeTo(0.70692, 1e-5));
       expect(singleUVtoST(1), closeTo(1, 1e-9));
@@ -26,7 +26,7 @@ void main() {
       expect(singleUVtoST(0.5), closeTo(0.79056, 1e-5));
     });
 
-    test('UVToST converts uv array to st array correctly', () {
+    test('uvToST converts uv array to st array correctly', () {
       expect(uvToST([0, 0]), [closeTo(0.5, 1e-9), closeTo(0.5, 1e-9)]);
       expect(uvToST([0.3333333333333333,-0.3333333333333333]), [closeTo(0.7071067811865476, 1e-9), closeTo(0.2928932188134524, 1e-9)]);
       expect(uvToST([1, 1]), [closeTo(1, 1e-9), closeTo(1, 1e-9)]);
