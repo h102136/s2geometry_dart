a314dbe849d6311f0aac26370f530652281e5f13
h102136 <h102136@gmail.com>
2024-09-05 19:37:30 +0930

    face_uv update

--- test/face_uv_test.dart
+++ test/face_uv_test.dart
@@ -4,7 +4,6 @@ import 'package:s2geometry_dart/src/face_uv.dart';
 void main() {
   group('largestAbsComponent', () {
     
-    // test for the largest absolute component
     test('should return the index of the largest absolute component', () {
       expect(largestAbsComponent([3.0, -4.0, 2.0]), 1);
       expect(largestAbsComponent([-5.0, 2.0, 3.0]), 0);
@@ -13,7 +12,7 @@ void main() {
   });
 
   group('faceXYZToUV', () {
-    test('should convert xyz to uv coordinates correctly', () {
+    test('should convert xyz to uv correctly', () {
       expect(faceXYZToUV(0, [1.0, 2.0, 3.0]), [2.0, 3.0]);
       expect(faceXYZToUV(1, [1.0, 2.0, 3.0]), [-0.5, 1.5]);
       expect(faceXYZToUV(2, [1.0, 2.0, 3.0]), [-0.3333333333333333, -0.6666666666666666]);
@@ -21,7 +20,7 @@ void main() {
   });
 
   group('xyzToFaceUV', () {
-    test('should convert xyz (1.0, 2.0, 3.0)to face and uv coordinates correctly', () {
+    test('should convert xyz (1.0, 2.0, 3.0)to face and uv correctly', () {
       var result = xyzToFaceUV([1.0, 2.0, 3.0]);
       expect(result[0], 2);
       expect(result[1][0], closeTo(-0.3333333333333333, 1e-9));
@@ -30,7 +29,7 @@ void main() {
   });
 
   group('xyzToFaceUV', () {
-    test('should convert xyz to face and uv coordinates correctly', () {
+    test('should convert xyz(-1.0, -2.0, -3.0) to face and uv  correctly', () {
       var result = xyzToFaceUV([-1.0, -2.0, -3.0]);
       expect(result[0], 5);
       expect(result[1][0], closeTo(-0.6666666666666667, 1e-9));
@@ -39,7 +38,7 @@ void main() {
   });
   
   group('faceUVToXYZ', () {
-    test('should convert uv coordinates to xyz correctly', () {
+    test('should convert uv to xyz correctly', () {
       expect(faceUVToXYZ(0, [2.0, 3.0]), [1.0, 2.0, 3.0]);
       expect(faceUVToXYZ(1, [-0.5, 1.5]), [0.5, 1.0, 1.5]);
       expect(faceUVToXYZ(2, [-3.0, -2.0]), [3.0, 2.0, 1.0]);
